6f4d089c1e57f4432ec8ddd01afd60362d1a2d3e
Akshar Sarvesh <aksharsarvesh@gmail.com>
2026-03-14 20:20:22 -0700

    look a little nicer

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-foreground">
-            PokeQuery
+            PokéQuery
           </h1>
           <form onSubmit={handleSubmit} className="flex w-full max-w-md flex-col gap-3">
             <input
